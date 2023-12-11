3a1dddb30ef119eda77f84a6058e1b6e6ccee7f1
shizhicheng <im.szc@live.com>
2023-12-11 11:25:40 +0800

    升级呼叫组件版本到 2.1.3，去除 resolve alias 配置

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-const path = require('path')
+// const path = require('path')
 
-module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-       'nertc-web-sdk': path.resolve(__dirname, 'node_modules/nertc-web-sdk'),
-      },
-    },
-  }
-}
+// module.exports = {
+//   configureWebpack: {
+//     resolve: {
+//       alias: {
+//        'nertc-web-sdk': path.resolve(__dirname, 'node_modules/nertc-web-sdk'),
+//       },
+//     },
+//   }
+// }
